7dd7a746d4277a113bce2e72e304a5e5029644ec
Sfen Helaers <sfenhelaers@gmail.com>
2016-08-29 14:52:25 +0200

    editor screen styling, hover effect, play button integration

--- src/app/containers/editor/editor.component.ts
+++ src/app/containers/editor/editor.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import { ROUTER_DIRECTIVES, Router } from '@angular/router';
+
+import { WheelComponent } from '../../components/wheel/wheel.component';
+
+import { SlidesService } from '../../services/slides.service';
 
 @Component({
     selector: 'editor',
     templateUrl: 'app/containers/editor/editor.component.html' ,
     styleUrls: ['app/containers/editor/editor.component.css'] ,
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, WheelComponent],
+    providers: [SlidesService]
 })
 
 export class EditorComponent {
-    message = 'EDITOR here';
+
+    private totalSlides:number;
+    private slides:Array<any> = [];
+
+    constructor(
+        private slidesService: SlidesService,
+        private router: Router) { 
+
+        }
+
+    ngOnInit() {
+        this.slides = this.slidesService.getAll();
+        this.totalSlides = this.slides.length;
+    }
 }
